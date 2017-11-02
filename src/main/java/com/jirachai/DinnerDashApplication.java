fc2a82fbcf5737642942bd75a6f6dd7bed4d5d57
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-11-02 18:40:20 +0700

    Initial commit

---
+++ src/main/java/com/jirachai/DinnerDashApplication.java
@@ -0,0 +1,12 @@
+package com.jirachai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerDashApplication.class, args);
+	}
+}
